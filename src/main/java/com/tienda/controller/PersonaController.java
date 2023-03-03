61ae5bacab7881e46a05623e56ca6875f959b2cc
AndresLeonJ <105888687+AndresLeonJ@users.noreply.github.com>
2023-03-02 22:30:24 -0600

    Semana 7

--- src/main/java/com/tienda/controller/PersonaController.java
+++ src/main/java/com/tienda/controller/PersonaController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
@@ -46,6 +47,14 @@ public class PersonaController {
         return "redirect:/persona";
     }
 
+    @GetMapping("/editPersona/{id}")
+    public String editPersona(@PathVariable("id")Long idPersona, Model model){
+        Persona persona = personaService.getPersonaById(idPersona);
+        List<Pais> listaPaises = paisService.listCountry();
+        model.addAttribute("persona", persona);
+        model.addAttribute("paises", listaPaises);
+        return "crear";
+    }
 }
 
 
